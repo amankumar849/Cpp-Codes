f5786d6dad4d25ab8ee184b786d41005340f42b8
Aman <amankumar849000@gmail.com>
2025-05-22 01:13:49 +0530

    day 3

--- Strings/PushPop.cpp
+++ Strings/PushPop.cpp
@@ -2,14 +2,16 @@
 using namespace std;
 int main(){
     string s = "Aman";
-    cout<<s<<endl;     //Aman
+    cout<<s<<endl;               //Aman
+
+
     s.push_back('k');
-    cout<<s<<endl;     //Amank
+    cout<<s<<endl;               //Amank
     s.push_back('u');
-    cout<<s<<endl;     //Amanku
+    cout<<s<<endl;               //Amanku
 
     s.pop_back();    
-    cout<<s<<endl;     //Amank
+    cout<<s<<endl;               //Amank
     s.pop_back();
     cout<<s<<endl;              //Aman
 
@@ -17,10 +19,10 @@ int main(){
     cout<<s<<endl;              //Aman kumar
 
 
-    cout<<s.length()<<endl;    // 10
+    cout<<s.length()<<endl;      // 10
 
-    
+
     s.clear();
-    cout<<s.length(); 
+    cout<<s.length();             // 0
 
 }
